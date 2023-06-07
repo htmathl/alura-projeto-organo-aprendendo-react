a58555659764342f52db620d08787887de6d074f
Matheus <matheus.domingues126@gmail.com>
2023-06-07 12:26:26 -0300

    Subir módulo 2

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,14 +1,34 @@
 import './Forms.css';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
 
 const Forms = () => {
+
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ];
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log("Submetido");
+    }
+
     return(
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para criar o card do colaborador</h2>
-            <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-            <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-            <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
+            <CampoTexto label="Imagem"  placeholder="Digite o endereço da imagem"/>
+            <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+            <Botao> Criar Card </Botao>
             </form>
         </section>
     );
